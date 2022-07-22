edd31cdefbe1a6deec3ee428842f55d5410256c0
haoblackj <yagu001@scientia-lebensborn.com>
2022-07-22 15:18:14 +0900

    fix(install.sh): 毎回Pullしてくれるようにしたよ

--- install.sh
+++ install.sh
@@ -3,10 +3,31 @@
 # 未定義な変数があったら途中で終了する
 set -u
 
+DOT_DIRECTORY="${HOME}/dotfiles"
+DOT_TARBALL="https://github.com/haoblackj/dotfiles/tarball/master"
+REMOTE_URL="git@github.com:haoblackj/dotfiles.git"
+# ディレクトリがなければダウンロード（と解凍）する
+if [ ! -d ${DOT_DIRECTORY} ]; then
+  echo "Downloading dotfiles..."
+  mkdir ${DOT_DIRECTORY}
+
+  if type "git" > /dev/null 2>&1; then
+    git clone --recursive "${REMOTE_URL}" "${DOT_DIRECTORY}"
+  else
+    curl -fsSLo ${HOME}/dotfiles.tar.gz ${DOT_TARBALL}
+    tar -zxf ${HOME}/dotfiles.tar.gz --strip-components 1 -C ${DOT_DIRECTORY}
+    rm -f ${HOME}/dotfiles.tar.gz
+  fi
+
+  echo $(tput setaf 2)Download dotfiles complete!. ✔︎$(tput sgr0)
+fi
+
 # 今のディレクトリ
 # dotfilesディレクトリに移動する
 BASEDIR=$(dirname $0)
 cd $BASEDIR
+
+git pull
 
 mkdir -p ~/.config/gh
 
