dd0f26ada911fa46c52aa224e3fd156414071cf4
haoblackj <17177994+haoblackj@users.noreply.github.com>
2023-01-02 00:54:05 +0900

    fix(zsh/plugins.zsh): add plugins

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -1,6 +1,13 @@
 sh -c "$(curl -fsSL git.io/get-zi)" --
 
+zinit light zsh-users/zsh-syntax-highlighting
+
 zinit light zsh-users/zsh-autosuggestions
+
+zinit light zsh-users/zsh-completions
 
 zinit load atusy/gh-fzf
 
+# Load the pure theme, with zsh-async library that's bundled with it.
+zinit ice pick"async.zsh" src"pure.zsh"
+zinit light sindresorhus/pure
