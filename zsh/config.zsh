9e17928d3ca64443b51f55d7b26cbd8d78b78def
haoblackj <17177994+haoblackj@users.noreply.github.com>
2023-01-01 19:44:00 +0900

    fzf関数

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -1,1 +1,10 @@
 setopt no_beep
+
+function ghclone(){
+  gh repo clone $(gh repo list $1 --json nameWithOwner -q '.[].nameWithOwner' | fzf)
+}
+
+function ghview(){
+  gh repo view --web $(gh repo list $1 --json nameWithOwner -q '.[].nameWithOwner' | fzf)
+}
+
