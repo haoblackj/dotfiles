fb7b7648ad0dd5e5835127011bd88c6f737cf735
haoblackj <17177994+haoblackj@users.noreply.github.com>
2023-01-02 20:30:02 +0900

    p10k configure and pure deleted

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -9,5 +9,5 @@ zinit light zsh-users/zsh-completions
 zinit load atusy/gh-fzf
 
 # Load the pure theme, with zsh-async library that's bundled with it.
-zinit ice pick"async.zsh" src"pure.zsh"
-zinit light sindresorhus/pure
+#zinit ice pick"async.zsh" src"pure.zsh"
+#zinit light sindresorhus/pure
