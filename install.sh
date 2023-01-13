547989cde06bd7bebac437a173a6fa4465dc9d0e
haoblackj <17177994+haoblackj@users.noreply.github.com>
2023-01-13 16:44:26 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -63,6 +63,7 @@ if [ -n "$(which wslpath)" ]; then
   echo "動作環境はWSLです"
   # wsl.confに対して
   sudo ln -snfv ${PWD}/wsl.conf /etc/wsl.conf
+  sudo ln -snfv ${PWD}/resolv.conf /etc/wsl.conf
   #sudo apt-get update -y && sudo apt-get upgrade -y
   WINHOME=/mnt/c/Users/$(cmd.exe /c "echo %USERNAME%" 2>/dev/null | tr -d '\r')
   echo ${WINHOME}
