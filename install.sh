cda22bdb30701a0c18a9a9c00c87a3f69d46a17a
haoblackj <17177994+haoblackj@users.noreply.github.com>
2023-01-15 14:51:28 +0900

    peco rawfile download dir modified

--- install.sh
+++ install.sh
@@ -164,6 +164,7 @@ type peco >/dev/null 2>&1
   else
   sudo add-apt-repository ppa:longsleep/golang-backports
   sudo apt install golang-go -y
+  cd ${PWD}
   wget https://github.com/peco/peco/releases/download/v0.5.7/peco_linux_386.tar.gz
   tar xzvf peco_linux_386.tar.gz
   cd peco_linux_386
