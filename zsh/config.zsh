5e3f40094da7c0984ab07c197992df5dd3e16a1a
haoblackj <17177994+haoblackj@users.noreply.github.com>
2023-02-12 19:11:07 +0900

    nvm configuration

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -65,3 +65,7 @@ function ghq_peco {
 }
 zle -N ghq_peco
 bindkey '^]' ghq_peco
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
