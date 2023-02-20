8db0106683ded052832b278b5b823621b9f61efb
haoblackj <17177994+haoblackj@users.noreply.github.com>
2023-02-20 10:29:28 +0900

    Update postinstall.sh

--- postinstall.sh
+++ postinstall.sh
@@ -1,3 +1,5 @@
+# Install NVM
+curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | sh
 go install github.com/x-motemen/ghq@latest
 gh auth login
 gh extension install kawarimidoll/gh-q
