78d8dbefad1a246178462ba216c9d22e8736936d
haoblackj <17177994+haoblackj@users.noreply.github.com>
2023-04-24 14:05:56 +0900

    zi to zinit

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -1,13 +1,6 @@
-sh -c "$(curl -fsSL git.io/get-zi)" --
-
+# プラグインのインストールとロード
 zinit light zsh-users/zsh-syntax-highlighting
-
 zinit light zsh-users/zsh-autosuggestions
-
 zinit light zsh-users/zsh-completions
-
-zinit load atusy/gh-fzf
-
-# Load the pure theme, with zsh-async library that's bundled with it.
-#zinit ice pick"async.zsh" src"pure.zsh"
-#zinit light sindresorhus/pure
+zinit light atusy/gh-fzf
+zinit light romkatv/powerlevel10k
