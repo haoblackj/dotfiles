b21c6e48d60b16cd7db9f2a46155b1a8c17bcc43
haoblackj <17177994+haoblackj@users.noreply.github.com>
2023-04-25 10:20:12 +0900

    feat(zsh/alias.zsh): add aliases

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -2,3 +2,7 @@ alias sudo='sudo '
 alias systemctl='/usr/libexec/nslogin systemctl'
 alias gco="git checkout"
 alias gcob="git checkout -b"
+alias ghprc="gh prc"
+alias ghprw="gh prw"
+alias ghrw="gh rw"
+alias ghauth="gh auth login -h github.com -p https -w"
