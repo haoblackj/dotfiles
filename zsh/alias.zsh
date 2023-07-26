28356e2ea2e75728742c90f7a7cebe148d8f9969
haoblackj <17177994+haoblackj@users.noreply.github.com>
2023-07-26 16:25:20 +0900

    feat(zsh/alias.zsh): ansible関連エイリアスを追加

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -6,3 +6,5 @@ alias ghprc="gh prc"
 alias ghprw="gh prw"
 alias ghrw="gh rw"
 alias ghauth="gh auth login -h github.com -p https -w"
+alias ansible='docker container run --rm -it --mount type=bind,src="$(pwd)",dst=/app ansible ansible'
+alias ansible-playbook='docker container run --rm -it --mount type=bind,src="$(pwd)",dst=/app ansible ansible-playbook'
