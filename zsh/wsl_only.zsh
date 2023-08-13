7459f9a53b8c1d64df2b648c9e1199ec189049ae
haoblackj <17177994+haoblackj@users.noreply.github.com>
2023-08-13 14:04:14 +0900

    fix(zsh/wsl_only.zsh): bitwardenはいらんよね

--- zsh/wsl_only.zsh
+++ zsh/wsl_only.zsh
@@ -14,23 +14,3 @@ function ghq_peco {
 }
 zle -N ghq_peco
 bindkey '^]' ghq_peco
-
-function bw_set_session {
-  local session_key
-  session_key="$(bw unlock --raw)"
-  export BW_SESSION="$session_key"
-  echo "Session key set."
-}
-
-function bw_get_item {
-  if [[ -z "$BW_SESSION" ]]; then
-    echo "No session key found. Running bw_set_session..."
-    bw_set_session
-  fi
-
-  local item_name="$1"
-  local item_field="$2"
-  local item_value
-  item_value="$(bw get $item_field $item_name --session $BW_SESSION)"
-  echo "$item_value"
-}
