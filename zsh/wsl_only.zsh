792308f420f8f0c1665d080ece2badb78adb6318
haoblackj <17177994+haoblackj@users.noreply.github.com>
2023-10-31 11:32:52 +0900

    fix(zsh/wsl_only.zsh): DENOを追加したよ

--- zsh/wsl_only.zsh
+++ zsh/wsl_only.zsh
@@ -2,6 +2,8 @@
 export PATH="$HOME/.local/bin:$PATH"
 export PATH="$HOME/.tfenv/bin:$PATH"
 export LD_LIBRARY_PATH=/usr/lib/x86_64-linux-gnu:$LD_LIBRARY_PATH
+export DENO_INSTALL="/home/yagu001/.deno"
+export PATH="$DENO_INSTALL/bin:$PATH"
 
 # asdf Path設定
 . "$HOME/.asdf/asdf.sh"
