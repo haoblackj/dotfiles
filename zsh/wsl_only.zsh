ae4c58298f27842fcb99c07540145a51e2b7f50a
haoblackj <17177994+haoblackj@users.noreply.github.com>
2023-11-04 12:11:03 +0900

    feat: Add win32yank installation and update PATH in WSL-only script
    This commit adds a new section to install win32yank, a clipboard manipulation program, in the WSL-only script. It also updates the PATH variable to include $HOME/bin and /usr/local/bin for better accessibility.

--- zsh/wsl_only.zsh
+++ zsh/wsl_only.zsh
@@ -4,6 +4,7 @@ export PATH="$HOME/.tfenv/bin:$PATH"
 export LD_LIBRARY_PATH=/usr/lib/x86_64-linux-gnu:$LD_LIBRARY_PATH
 export DENO_INSTALL="/home/yagu001/.deno"
 export PATH="$DENO_INSTALL/bin:$PATH"
+export PATH=$HOME/bin:/usr/local/bin:$PATH
 
 # asdf Path設定
 . "$HOME/.asdf/asdf.sh"
