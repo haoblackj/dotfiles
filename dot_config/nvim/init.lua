7cc49091c11b4d2c9d06436d831e281126a71786
haoblackj <17177994+haoblackj@users.noreply.github.com>
2023-12-05 11:14:43 +0900

    feat: Add neovim main configuration
    This commit adds the main configuration for Neovim, including the setup of plugins, deno path, leader key mapping, disabling backup and swap files, enabling indent visualization, setting encoding, mapping 'jj' and 'っj' to ESC key, localizing help, specifying spzenhan.exe path, creating autocmds for restoring and saving IME state, displaying line numbers, setting backspace behavior, automatically closing brackets, configuring cursor shape, yanking to clipboard, setting color scheme, mapping Ctrl+n to toggle file tree view, enabling syntax highlighting, and forcing syntax highlighting for *.txt files.
    
    The terminal escape key is also mapped to <C-\\><C-n> for leaving insert mode in the terminal.

---
+++ dot_config/nvim/init.lua
@@ -0,0 +1,92 @@
+-- Neovimの主要設定
+-- ここには他の一般的な設定やキーマッピングなどを追加できます
+
+-- pluginsフォルダ内のsetup_plugin.luaをrequireする
+-- 相対パスは 'plugins.setup_plugin' となる（Luaではドットを使って階層を表現します）
+require('plugins.setup_plugin')
+
+-- 他の設定や機能をここに追加することができます
+-- 例: require('some_other_config')
+
+-- denoのパス設定
+vim.g['denops#deno'] = vim.fn.expand('$HOME') .. '/.deno/bin/deno'
+
+-- leaderキーの設定
+vim.g.mapleader = " "
+
+-- バックアップファイルとスワップファイルを作らない設定
+vim.opt.backup = false
+vim.opt.swapfile = false
+
+-- インデント可視化設定
+vim.opt.list = true
+vim.opt.listchars = 'tab:»-,trail:-,eol:↲,extends:»,precedes:«,nbsp:%'
+
+-- エンコーディング設定
+vim.opt.encoding = 'utf-8'
+vim.scriptencoding('utf-8')
+
+-- jキーを二度押しでESCキーとして機能するようにする
+vim.api.nvim_set_keymap('i', 'jj', '<Esc>', {silent = true})
+vim.api.nvim_set_keymap('i', 'っj', '<ESC>', {silent = true})
+
+-- ヘルプを日本語化
+vim.opt.helplang = 'ja'
+
+-- spzenhan.exe のパスを指定
+vim.g.spzenhan_executable = '/mnt/c/WorkTmp/spzenhan.vim/zenhan/spzenhan.exe'
+
+-- インサートモードに入るたびに、直前のIMEの状態を復元する
+vim.api.nvim_create_autocmd('InsertEnter', {
+    pattern = '*',
+    command = 'call system(g:spzenhan_executable . " " . g:previous_ime_state)'
+})
+
+-- インサートモードを離れるたびに、現在のIMEの状態を保存し、IMEを無効にする
+vim.api.nvim_create_autocmd('InsertLeave', {
+    pattern = '*',
+    command = 'let g:previous_ime_state = system(g:spzenhan_executable . " --get")[:-2] | call system(g:spzenhan_executable . " 0")'
+})
+
+-- 行番号の表示
+vim.opt.number = true
+
+-- 挿入モードでバックスペースキーの設定
+vim.opt.backspace = {'indent', 'eol', 'start'}
+
+-- 自動でカッコ等を閉じる設定
+vim.api.nvim_set_keymap('i', '{', '{}<LEFT>', {})
+vim.api.nvim_set_keymap('i', '[', '[]<LEFT>', {})
+vim.api.nvim_set_keymap('i', '(', '()<LEFT>', {})
+vim.api.nvim_set_keymap('i', '"', '""<LEFT>', {})
+vim.api.nvim_set_keymap('i', "'", "''<LEFT>", {})
+vim.api.nvim_set_keymap('i', '「', '「」<LEFT>', {})
+vim.api.nvim_set_keymap('i', '『', '『』<LEFT>', {})
+vim.api.nvim_set_keymap('i', '〈', '〈〉<LEFT>', {})
+vim.api.nvim_set_keymap('i', '《', '《》<LEFT>', {})
+
+-- 棒状カーソルの設定
+vim.g.t_SI = "\<Esc>]50;CursorShape=1\x7"
+vim.g.t_EI = "\<Esc>]50;CursorShape=0\x7"
+vim.api.nvim_set_keymap('i', '<Esc>', '<Esc>lh', {})
+
+-- ヤンクするとクリップボードに保存される設定
+vim.opt.clipboard:append('unnamedplus')
+
+-- カラーテーマの設定
+vim.cmd('colorscheme codedark')
+
+-- Ctrl+nでファイルツリーを表示/非表示するキーマップ
+vim.api.nvim_set_keymap('n', '<C-n>', ':Fern . -reveal=% -drawer -toggle -width=40<CR>', {})
+
+-- シンタックスハイライトを有効にする設定
+vim.cmd('filetype plugin indent on')
+
+-- シンタックスハイライト強制設定
+vim.api.nvim_create_autocmd({'BufRead', 'BufNewFile'}, {
+    pattern = '*.txt',
+    command = 'set filetype=txtjp'
+})
+
+-- Terminalのインサートモードからの離脱をescキーにマッピング
+vim.api.nvim_set_keymap('t', '<Esc>', '<C-\\><C-n>', {})
