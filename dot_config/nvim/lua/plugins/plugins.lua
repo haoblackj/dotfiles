9447c262de2b842adbef56e3a10780e545db81b1
haoblackj <17177994+haoblackj@users.noreply.github.com>
2023-12-15 14:05:55 +0900

    Add clipboard settings, keymaps, writing configuration, and plugins

---
+++ dot_config/nvim/lua/plugins/plugins.lua
@@ -0,0 +1,8 @@
+-- lazy.nvimを使ってNeovimのプラグインを管理する
+require("lazy").setup({
+  "vim-denops/denops.vim",
+  "lambdalisue/kensaku.vim",
+  "lambdalisue/kensaku-search.vim",
+  "yuki-yano/fuzzy-motion.vim",
+  'echasnovski/mini.nvim',
+})
