20e81df5a5bc0aa71c57b245392d658a8d5ac677
haoblackj <17177994+haoblackj@users.noreply.github.com>
2023-12-15 23:02:02 +0900

    Add basic configuration for Neovim

---
+++ dot_config/nvim/lua/custom/init.lua
@@ -0,0 +1,50 @@
+-- Desc: 基本的な設定を行う
+-- txtjpの設定
+vim.api.nvim_create_autocmd({"BufRead", "BufNewFile"}, {
+    pattern = "*.txt",
+    command = "set filetype=txtjp",
+  })
+
+  -- キーマッピングの設定
+  local map = vim.keymap.set
+  local opts = { noremap = true, silent = true }
+
+  -- リーダーキーをスペースに設定
+  vim.g.mapleader = ' '
+
+  -- jキーを二度押しでESCキーと同様に扱う
+  map('i', 'jj', '<Esc>', opts)
+  map('i', 'っj', '<ESC>', opts)
+
+  -- 自動でカッコ等を閉じる
+  map('i', '{', '{}<Left>', opts)
+  map('i', '[', '[]<Left>', opts)
+  map('i', '(', '()<Left>', opts)
+  map('i', '"', '""<Left>', opts)
+  map('i', "'", "''<Left>", opts)
+  map('i', '「', '「」<Left>', opts)
+  map('i', '『', '『』<Left>', opts)
+  map('i', '〈', '〈〉<Left>', opts)
+  map('i', '《', '《》<Left>', opts)
+
+  -- 'S'を押すと、画面内の文字列に英数字入力でfuzzymotionできるようにする
+  map('n', 'S', ':FuzzyMotion<CR>', opts)
+
+  -- 論理行と表示行の移動をスワップする（VSCodeではスキップ）
+  if not vim.g.vscode then
+  map('n', 'k', 'gk', opts)
+  map('n', 'gk', 'k', opts)
+  map('n', 'j', 'gj', opts)
+  map('n', 'gj', 'j', opts)
+  map('n', '0', 'g0', opts)
+  map('n', 'g0', '0', opts)
+  map('n', '^', 'g^', opts)
+  map('n', 'g^', '^', opts)
+  map('n', '$', 'g$', opts)
+  map('n', 'g$', '$', opts)
+  map('i', '<Down>', '<C-o>gj', opts)
+  map('i', '<Up>', '<C-o>gk', opts)
+  end
+
+  -- lazygitを呼び出すキーマップ
+  map('n', '<leader>gg', ':LazyGit<CR>', opts)
