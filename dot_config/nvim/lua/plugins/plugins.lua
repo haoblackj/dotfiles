d269bfb2d7414e712edd1a8acf833f472b26e056
haoblackj <17177994+haoblackj@users.noreply.github.com>
2023-12-15 16:13:04 +0900

    Remove clipboard and writing configuration files, update plugin dependencies

--- dot_config/nvim/lua/plugins/plugins.lua
+++ dot_config/nvim/lua/plugins/plugins.lua
@@ -1,8 +1,23 @@
 -- lazy.nvimを使ってNeovimのプラグインを管理する
-require("lazy").setup({
-  "vim-denops/denops.vim",
-  "lambdalisue/kensaku.vim",
-  "lambdalisue/kensaku-search.vim",
-  "yuki-yano/fuzzy-motion.vim",
-  'echasnovski/mini.nvim',
-})
+return {
+  {
+      'lambdalisue/kensaku-search.vim',
+      dependencies = {
+          {
+              'lambdalisue/kensaku.vim',
+              dependencies = {
+                  {
+                      'vim-denops/denops.vim',
+                      -- vim-denops/denops.vim の追加の設定や関数をここに記述
+                  }
+              },
+              -- lambdalisue/kensaku.vim の追加の設定や関数をここに記述
+          }
+      },
+      -- lambdalisue/kensaku-search.vim の追加の設定や関数をここに記述
+  },
+  {
+      'yuki-yano/fuzzy-motion.vim',
+      -- yuki-yano/fuzzy-motion.vim の追加の設定や関数をここに記述
+  }
+}
