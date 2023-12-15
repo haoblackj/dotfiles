5227f9e8ba4064b465981ddecd49aa33886123e8
haoblackj <17177994+haoblackj@users.noreply.github.com>
2023-12-15 23:13:31 +0900

    Update init.lua with various configurations

--- dot_config/nvim/lua/custom/init.lua
+++ dot_config/nvim/lua/custom/init.lua
@@ -5,46 +5,83 @@ vim.api.nvim_create_autocmd({"BufRead", "BufNewFile"}, {
     command = "set filetype=txtjp",
   })
 
-  -- キーマッピングの設定
-  local map = vim.keymap.set
-  local opts = { noremap = true, silent = true }
+-- disable netrw at the very start of your init.lua
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
 
-  -- リーダーキーをスペースに設定
-  vim.g.mapleader = ' '
+-- set termguicolors to enable highlight groups
+vim.opt.termguicolors = true
 
-  -- jキーを二度押しでESCキーと同様に扱う
-  map('i', 'jj', '<Esc>', opts)
-  map('i', 'っj', '<ESC>', opts)
+-- empty setup using defaults
+require("nvim-tree").setup()
 
-  -- 自動でカッコ等を閉じる
-  map('i', '{', '{}<Left>', opts)
-  map('i', '[', '[]<Left>', opts)
-  map('i', '(', '()<Left>', opts)
-  map('i', '"', '""<Left>', opts)
-  map('i', "'", "''<Left>", opts)
-  map('i', '「', '「」<Left>', opts)
-  map('i', '『', '『』<Left>', opts)
-  map('i', '〈', '〈〉<Left>', opts)
-  map('i', '《', '《》<Left>', opts)
+-- OR setup with some options
+require("nvim-tree").setup({
+  sort = {
+    sorter = "case_sensitive",
+  },
+  view = {
+    width = 30,
+  },
+  renderer = {
+    group_empty = true,
+  },
+  filters = {
+    dotfiles = true,
+  },
+})
 
-  -- 'S'を押すと、画面内の文字列に英数字入力でfuzzymotionできるようにする
-  map('n', 'S', ':FuzzyMotion<CR>', opts)
+--kensaku-search
+vim.keymap.set('c', '<CR>', '<Plug>(kensaku-search-replace)<CR>')
 
-  -- 論理行と表示行の移動をスワップする（VSCodeではスキップ）
-  if not vim.g.vscode then
-  map('n', 'k', 'gk', opts)
-  map('n', 'gk', 'k', opts)
-  map('n', 'j', 'gj', opts)
-  map('n', 'gj', 'j', opts)
-  map('n', '0', 'g0', opts)
-  map('n', 'g0', '0', opts)
-  map('n', '^', 'g^', opts)
-  map('n', 'g^', '^', opts)
-  map('n', '$', 'g$', opts)
-  map('n', 'g$', '$', opts)
-  map('i', '<Down>', '<C-o>gj', opts)
-  map('i', '<Up>', '<C-o>gk', opts)
-  end
+--furzy-motion
+vim.keymap.set('n', 'S', '<cmd>FuzzyMotion<CR>')
+vim.cmd("let g:fuzzy_motion_matchers = ['kensaku', 'fzf']")
 
-  -- lazygitを呼び出すキーマップ
-  map('n', '<leader>gg', ':LazyGit<CR>', opts)
+-- Clipboard settings for WSL
+vim.opt.clipboard = "unnamedplus"
+if vim.fn.has("wsl") == 1 then
+    vim.g.clipboard = {
+        name = 'WslClipboard',
+        copy = {
+            ['+'] = 'xsel -bi',
+            ['*'] = 'xsel -bi',
+        },
+        paste = {
+            ['+'] = 'xsel -bo',
+            ['*'] = function() return vim.fn.systemlist('xsel -bo | tr -d "\r"') end,
+        },
+        cache_enabled = 1,
+    }
+end
+
+-- spzenhan.exe のパスを指定
+vim.g.spzenhan_executable = '/mnt/c/WorkTmp/spzenhan.vim/zenhan/spzenhan.exe'
+
+-- グローバル変数を追跡するための初期化
+vim.g.previous_ime_state = 0
+
+-- インサートモードに入るたび、直前のIMEの状態を復元
+vim.api.nvim_create_autocmd("InsertEnter", {
+    pattern = "*",
+    callback = function()
+        vim.fn.system(vim.g.spzenhan_executable .. ' ' .. vim.g.previous_ime_state)
+    end
+})
+
+-- インサートモードを離れるたび、IMEの状態を保存し無効化
+vim.api.nvim_create_autocmd("InsertLeave", {
+    pattern = "*",
+    callback = function()
+        vim.g.previous_ime_state = vim.fn.system(vim.g.spzenhan_executable .. ' --get'):gsub("\n", "")
+        vim.fn.system(vim.g.spzenhan_executable .. ' 0')
+    end
+})
+
+-- .txt ファイルの折り返しを有効にする
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = "txtjp",
+    callback = function()
+        vim.opt_local.wrap = true
+    end
+})
