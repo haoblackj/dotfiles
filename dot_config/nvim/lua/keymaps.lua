8fadef9ee2ebc14e4a5b2fc28e500bec4600abdd
haoblackj <17177994+haoblackj@users.noreply.github.com>
2024-11-05 22:48:48 +0900

    Update keymaps.lua with improved navigation mappings

--- dot_config/nvim/lua/keymaps.lua
+++ dot_config/nvim/lua/keymaps.lua
@@ -1,3 +1,9 @@
 -- bufferlines関連
 vim.keymap.set("n", "<C-h>", "<cmd>bprev<CR>")
 vim.keymap.set("n", "<C-l>", "<cmd>bnext<CR>")
+
+-- 設定で通常の上下移動を表示行に置き換える
+vim.api.nvim_set_keymap('n', 'j', 'gj', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', 'k', 'gk', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', 'j', 'gj', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', 'k', 'gk', { noremap = true, silent = true })
