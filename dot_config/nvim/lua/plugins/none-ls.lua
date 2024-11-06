78ff0b64f452128fdeca4e20dd76f10059bceb76
haoblackj <17177994+haoblackj@users.noreply.github.com>
2024-11-06 13:57:29 +0900

    Update lazy.nvim plugin configuration and keymapping

--- dot_config/nvim/lua/plugins/none-ls.lua
+++ dot_config/nvim/lua/plugins/none-ls.lua
@@ -1,6 +1,30 @@
 return {
   'nvimtools/none-ls.nvim',
   dependencies = 'nvim-lua/plenary.nvim',
-  config = true,
+  config = function()
+    local null_ls = require("null-ls")
+    local helpers = require("null-ls.helpers")
+
+    null_ls.setup({
+        sources = {
+            null_ls.builtins.diagnostics.textlint.with({
+                command = "yarn",
+                args = { "textlint", "--format", "unix", "--stdin" },
+                filetypes = { "markdown", "text" }, -- 使用するファイルタイプ
+                condition = function(utils)
+                    return utils.root_has_file(".textlintrc")
+                end,
+            }),
+        },
+    })
+
+    -- 保存時に自動的にlintを実行するautocmd設定
+    vim.api.nvim_create_autocmd("BufWritePost", {
+        pattern = { "*.md", "*.txt" },
+        callback = function()
+            vim.lsp.buf.format({ async = true })
+        end,
+    })
+  end,
 }
 
