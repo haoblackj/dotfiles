9a5ad305594cdb1d0394c5ba32c46bb616c651eb
haoblackj <17177994+haoblackj@users.noreply.github.com>
2024-11-07 01:06:37 +0900

    Update none-ls plugin configuration and keymapping

--- dot_config/nvim/lua/plugins/none-ls.lua
+++ dot_config/nvim/lua/plugins/none-ls.lua
@@ -1,30 +1,24 @@
 return {
-  'nvimtools/none-ls.nvim',
-  dependencies = 'nvim-lua/plenary.nvim',
-  config = function()
-    local null_ls = require("null-ls")
-    local helpers = require("null-ls.helpers")
+    "nvimtools/none-ls.nvim",
+    config = function()
+        local none_ls = require("null-ls")
 
-    null_ls.setup({
-        sources = {
-            null_ls.builtins.diagnostics.textlint.with({
-                command = "yarn",
-                args = { "textlint", "--format", "unix", "--stdin" },
-                filetypes = { "markdown", "text" }, -- 使用するファイルタイプ
-                condition = function(utils)
-                    return utils.root_has_file(".textlintrc")
-                end,
-            }),
-        },
-    })
+        none_ls.setup({
+            sources = {
+                none_ls.builtins.diagnostics.textlint.with({
+                    filetypes = { "text" }
+                }),
+                none_ls.builtins.formatting.textlint.with({
+                    filetypes = { "text" }
+                }),
+            },
+        })
 
-    -- 保存時に自動的にlintを実行するautocmd設定
-    vim.api.nvim_create_autocmd("BufWritePost", {
-        pattern = { "*.md", "*.txt" },
-        callback = function()
-            vim.lsp.buf.format({ async = true })
-        end,
-    })
-  end,
+        vim.api.nvim_create_autocmd("BufWritePost", {
+            pattern = { "*.md", "*.txt" },
+            callback = function()
+                vim.lsp.buf.format({ async = true })
+            end,
+        })
+    end,
 }
-
