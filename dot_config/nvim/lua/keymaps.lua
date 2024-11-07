4c30c1035cad9f6491082bc600142802dfb98dc2
haoblackj <17177994+haoblackj@users.noreply.github.com>
2024-11-07 16:27:04 +0900

    Update keymaps.lua for IME off setting and state transition

--- dot_config/nvim/lua/keymaps.lua
+++ dot_config/nvim/lua/keymaps.lua
@@ -7,3 +7,7 @@ vim.api.nvim_set_keymap('n', 'j', 'gj', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', 'k', 'gk', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('v', 'j', 'gj', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('v', 'k', 'gk', { noremap = true, silent = true })
+
+-- 状態遷移系
+vim.keymap.set("i", "jj", "<esc><cmd>w<CR>")
+vim.keymap.set("i", "kk", "<esc><cmd>w<CR>")
