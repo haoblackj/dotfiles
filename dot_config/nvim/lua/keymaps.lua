ada745adb032a2d6768601bbdd18515da1e88ccf
haoblackj <17177994+haoblackj@users.noreply.github.com>
2024-11-10 02:34:33 +0900

    feat: 矢印キーでも表示行を移動できるようにしたよ、ついでに現在行の移動を実装したよ

--- dot_config/nvim/lua/keymaps.lua
+++ dot_config/nvim/lua/keymaps.lua
@@ -8,6 +8,21 @@ vim.api.nvim_set_keymap('n', 'k', 'gk', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('v', 'j', 'gj', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('v', 'k', 'gk', { noremap = true, silent = true })
 
+-- 矢印キーでの上下移動も表示行に置き換える
+vim.api.nvim_set_keymap('n', '<Down>', 'gj', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<Up>', 'gk', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', '<Down>', 'gj', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', '<Up>', 'gk', { noremap = true, silent = true })
+
 -- 状態遷移系
 vim.keymap.set("i", "jj", "<esc><cmd>w<CR>")
 vim.keymap.set("i", "kk", "<esc><cmd>w<CR>")
+
+-- Alt + Shift + k で行を上に移動
+vim.keymap.set("n", "<A-K>", ":m .-2<CR>==", { noremap = true, silent = true })
+vim.keymap.set("n", "<A-<Up>>", ":m .-2<CR>==", { noremap = true, silent = true })
+
+-- Alt + Shift + j で行を下に移動
+vim.keymap.set("n", "<A-J>", ":m .+1<CR>==", { noremap = true, silent = true })
+vim.keymap.set("n", "<A-<Down>>", ":m .+1<CR>==", { noremap = true, silent = true })
+
