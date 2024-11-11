ccc4d66a866640bd25c4b614eea62c2082d6d6ca
haoblackj <17177994+haoblackj@users.noreply.github.com>
2024-11-11 15:36:53 +0900

    fix: 🐛 terminal preference at toggleterm

--- dot_config/nvim/lua/plugins/toggleterm.lua
+++ dot_config/nvim/lua/plugins/toggleterm.lua
@@ -9,7 +9,7 @@ return {
       hide_numbers = true, -- hide the number column in toggleterm buffers
       shade_filetypes = {},
       autochdir = false, -- when neovim changes it current directory the terminal will change it's own when next it's opened
-      shade_terminals = true, -- NOTE: this option takes priority over highlights specified so if you specify Normal highlights you should set this to false
+      shade_terminals = false, -- NOTE: this option takes priority over highlights specified so if you specify Normal highlights you should set this to false
       start_in_insert = true,
       insert_mappings = true, -- whether or not the open mapping applies in insert mode
       terminal_mappings = true, -- whether or not the open mapping applies in the opened terminals
@@ -24,7 +24,7 @@ return {
       -- This field is only relevant if direction is set to 'float'
       float_opts = {
         border = 'curved',
-        winblend = 3,
+        winblend = 0,
         title_pos = 'center',
       },
       winbar = {
