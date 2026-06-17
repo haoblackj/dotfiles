d500079af7e00e3afb39460a5cec7465882cbfbf
haoblackj <17177994+haoblackj@users.noreply.github.com>
2026-06-17 14:48:39 +0900

    feat: add WSL zsh launcher script for Ubuntu

---
+++ ubuntu-(wsl-zsh).ps1
@@ -0,0 +1,15 @@
+﻿# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Ubuntu (WSL zsh)
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon 🤖
+# @raycast.packageName Ubuntu-WSL
+# @raycast.description Launch Ubuntu
+
+# Documentation:
+# @raycast.author haoblackj
+# @raycast.authorURL https://raycast.com/haoblackj
+
+wt.exe -p "Ubuntu (WSL zsh)"
