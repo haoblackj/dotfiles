058c48839f89bf8e0ad209ed73cb730be121a053
haoblackj <17177994+haoblackj@users.noreply.github.com>
2026-07-22 11:30:46 +0900

    feat(nvim): persistence.nvimでバッファ・セッションを自動保存/復元
    herdrのHomebrewアップデートでサーバーを落とすとnvimのプロセスは
    失われるが、開いていたバッファ構成までは自動で戻らない。
    VimEnter時に引数なし起動なら前回セッションを自動ロードするようにした。

---
+++ dot_config/nvim/lua/plugins/persistence.lua
@@ -0,0 +1,14 @@
+return {
+  "folke/persistence.nvim",
+  event = "BufReadPre",
+  opts = {},
+  init = function()
+    vim.api.nvim_create_autocmd("VimEnter", {
+      callback = function()
+        if vim.fn.argc() == 0 then
+          require("persistence").load()
+        end
+      end,
+    })
+  end,
+}
